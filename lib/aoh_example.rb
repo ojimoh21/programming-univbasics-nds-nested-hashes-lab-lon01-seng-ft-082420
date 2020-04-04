15ecf194787240e683a91dc4719bd5ba89870a7d
Omowonuola Jimoh <ojimoh21@live.co.uk>
2020-04-04 20:24:37 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -11,7 +11,13 @@ def assembled_aoh
 end
 
 def literal_aoh
-  artist = [DON_G, JOELLE_VD, PAT_M, KATE_G, BRUCE_G]
+  [
+  {name: => "Don Gately", occupation => "Live-in Staff"},
+  {name => "Joelle van Dyke", occupation => "Radio Personality"},
+  {name => "Pat Monteseian", occupation => "Staff"},
+  {name => "Kate Gompert", occupation => "None"},
+  {name => "Bruce Green", occupation => "Fan of Mildred"}
+    ]
 end
 
 def aoh_lookup(aoh, row, key)
